88cea2b012a52858600a1d62e2a84d557af1777f
lukin999 <gianluca.maccioni.uk@gmail.com>
2018-10-24 03:18:57 +0100

    Add search boilerplate.
    Implement SearchView.OnQueryTextListener

--- app/src/main/java/com/example/luca/stockchartbeta/MainActivity.java
+++ app/src/main/java/com/example/luca/stockchartbeta/MainActivity.java
@@ -2,10 +2,16 @@ package com.example.luca.stockchartbeta;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    private static final String TAG = "MainActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,30 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        //return super.onCreateOptionsMenu(menu);
+
         getMenuInflater().inflate(R.menu.search, menu);
+
+        MenuItem searchButton = menu.findItem(R.id.search_button);
+        SearchView searchView = (SearchView) searchButton.getActionView();
+        searchView.setOnQueryTextListener(this);
+        return true;
+    }
+
+
+    @Override
+    public boolean onQueryTextSubmit(String s) {
+        Log.d(TAG, "onQueryTextSubmit: " + s);
+
+        return true;
+    }
+
+
+    @Override
+    public boolean onQueryTextChange(String s) {
+        Log.d(TAG, "onQueryTextChange: " + s);
 
         return true;
     }
