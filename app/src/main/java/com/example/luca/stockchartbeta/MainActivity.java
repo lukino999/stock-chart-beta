8dcd6bf9d53524429ea05c3ef3763123084d8fe7
lukin999 <gianluca.maccioni.uk@gmail.com>
2018-10-25 15:28:00 +0100

    Add nasdaq.csv and getArray()
    nasdaq.csv contains TickerSymbol, CompanyName pairs
    getArray will return an ArrayList of Stock from the .csv file

--- app/src/main/java/com/example/luca/stockchartbeta/MainActivity.java
+++ app/src/main/java/com/example/luca/stockchartbeta/MainActivity.java
@@ -8,6 +8,13 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+
 public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
     private static final String TAG = "MainActivity";
@@ -51,5 +58,28 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         Log.d(TAG, "onQueryTextChange: " + s);
 
         return true;
+    }
+
+    private ArrayList<Stock> getArray() {
+
+        // get inputStream from /res/raw/nasdaq.csv
+        InputStream inputStream = getResources().openRawResource(R.raw.nasdaq);
+
+        // get stream reader
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+        // iterate through the csv and add to items
+        ArrayList<Stock> items = new ArrayList<>();
+        String line;
+        try {
+            while ((line = reader.readLine()) != null) {
+                String[] value = line.split(",");
+                items.add(new Stock(value[1], value[0], "NASDAQ"));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return items;
     }
 }
