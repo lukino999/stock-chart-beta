1057f1c3932ce1ab95c5c1f48f69080524b7ff40
Luca <gianluca.maccioni@gmail.com>
2018-11-12 05:58:29 +0000

    Add AppExecutors

---
+++ app/src/main/java/com/example/luca/stockchartbeta/AppExecutors.java
@@ -0,0 +1,47 @@
+package com.example.luca.stockchartbeta;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class AppExecutors {
+
+    // For singleton instantiation
+    private static final Object LOCK = new Object();
+    private static AppExecutors sInstance;
+    private final Executor diskIO;
+    private final Executor mainThread;
+
+    public AppExecutors(Executor diskIO, Executor mainThread) {
+        this.diskIO = diskIO;
+        this.mainThread = mainThread;
+    }
+
+    public static AppExecutors getInstance() {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = new AppExecutors(Executors.newSingleThreadExecutor(),
+                        new MainThreadExecutor());
+            }
+        }
+        return sInstance;
+    }
+
+
+    //
+    public static class MainThreadExecutor implements Executor {
+
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+
+        @Override
+        public void execute(Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
+
+
+
+}
