3f617b22d531fba98ee711abc1a1cbdb32ac3975
Luca <gianluca.maccioni@gmail.com>
2018-11-12 00:21:35 +0000

    Convert Stock to entity

--- app/src/main/java/com/example/luca/stockchartbeta/Stock.java
+++ app/src/main/java/com/example/luca/stockchartbeta/Stock.java
@@ -6,19 +6,37 @@ package com.example.luca.stockchartbeta;
 //      Ticker symbol
 //      Exchange code name
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "stock")
 public class Stock {
 
+    @PrimaryKey(autoGenerate = true)
+    private int mID;
     private String mName;
     private String mSymbol;
     private String mExchange;
 
-    // constructor
+    // constructors
+    @Ignore
     public Stock(String name, String symbol, String exchange) {
         mName = name;
         mSymbol = symbol;
         mExchange = exchange;
     }
 
+    public Stock(int id, String name, String symbol, String exchange) {
+        mID = id;
+        mName = name;
+        mSymbol = symbol;
+        mExchange = exchange;
+    }
+
+    public int getId() {
+        return mID;
+    }
 
     public String getCompanyName() {
         return mName;
