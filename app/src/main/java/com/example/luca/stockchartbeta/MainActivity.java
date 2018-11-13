25e370a864065a811c09b2abcfc5071565c6ed75
lukino999 <gianluca.maccioni@gmail.com>
2018-11-13 06:17:37 +0000

    Retain search query on UI upon configuration change

--- app/src/main/java/com/example/luca/stockchartbeta/MainActivity.java
+++ app/src/main/java/com/example/luca/stockchartbeta/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -24,16 +25,24 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         StockListAdapter.ListItemClickListener {
 
     private static final String TAG = "MainActivity";
-
+    private static final String SEARCH_QUERY = "SEARCH_QUERY";
     private StockListAdapter mAdapter;
     private RecyclerView mStockList;
     private List<Stock> mFullList;
     private SearchView mSearchView;
+    private MenuItem mSearchButton;
+    private CharSequence mSearchQuery = "";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // retrieve the query
+        if (savedInstanceState != null) {
+            mSearchQuery = savedInstanceState.getCharSequence(SEARCH_QUERY);
+        }
 
         // setup action bar
         Toolbar toolbar = findViewById(R.id.my_toolbar);
@@ -53,8 +62,14 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         mStockList.setAdapter(mAdapter);
         //
         setupViewModel();
+    }
 
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // save the query
+        outState.putCharSequence(SEARCH_QUERY, mSearchQuery);
+        super.onSaveInstanceState(outState);
     }
 
 
@@ -64,15 +79,26 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         getMenuInflater().inflate(R.menu.search, menu);
 
         // get a reference to menu item search_button
-        MenuItem searchButton = menu.findItem(R.id.search_button);
+        mSearchButton = menu.findItem(R.id.search_button);
+
 
         // get a reference to the android.support.v7.widget.SearchView
-        mSearchView = (SearchView) searchButton.getActionView();
+        mSearchView = (SearchView) mSearchButton.getActionView();
         // sets the listener to SearchView.OnQueryTextListener
         mSearchView.setOnQueryTextListener(this);
 
         // returning true will display the menu
         return true;
+    }
+
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        // since configuration change is handled manually
+        // call setupViewModel()
+        setupViewModel();
     }
 
 
@@ -92,6 +118,13 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
                 Log.d(TAG, "retrieving data from LiveData in ViewModel");
                 mFullList = stocks;
                 mAdapter.setStockList(stocks);
+
+                // restore query to before configChange
+                Log.d(TAG, "onChanged: mSearchQuery -> " + mSearchQuery);
+                if (mSearchQuery != "") {
+                    mSearchButton.expandActionView();
+                    mSearchView.setQuery(mSearchQuery, true);
+                }
             }
         });
     }
@@ -109,19 +142,27 @@ public class MainActivity extends AppCompatActivity implements SearchView.OnQuer
         return true;
     }
 
+
+    // hide soft keyboard
     private void hideSoftKeyboard(Activity activity) {
-        InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        InputMethodManager inputMethodManager = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
     }
+
 
     @Override
     public boolean onQueryTextChange(String s) {
 
+        // update mSearchQuery. It will be retrieved after config change
+        mSearchQuery = s;
+
+        // filter list
         filterStockList(s);
 
         // job done
         return true;
     }
+
 
     private void filterStockList(String s) {
         // filtered list will contain search results
